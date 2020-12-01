2386a975661f05bb5799f7e8afa3b322957ac231
blminh <0306181245@caothang.edu.vn>
2020-12-01 19:56:28 +0700

    upload file

---
+++ app/src/main/java/com/example/kiemtraapi/APIGetting.java
@@ -0,0 +1,47 @@
+package com.example.kiemtraapi;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+public class APIGetting extends AsyncTask<Word, String, String> {
+
+    private Context m_con;
+
+    public APIGetting(Context con) {
+        m_con = con;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        Toast.makeText(m_con, "Start", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected String doInBackground(Word... word) {
+        if (word[0].getWord().isEmpty())
+            return APIWord.getWords();
+        else {
+            try {
+                return APIWord.addWord(word[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    protected void onProgressUpdate(String... values) {
+        super.onProgressUpdate(values);
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+        Toast.makeText(m_con, "Finished", Toast.LENGTH_SHORT).show();
+    }
+}
